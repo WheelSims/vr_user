b921c2dcbc711acfaada2ff93090b03956bdc2f4
Félix Chénier <chenier.felix@uqam.ca>
2023-12-01 13:28:07 -0500

    First commit for vr_user

---
+++ Contents/Scripts/KeyboardControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // React to keyboard
+        float linearSpeed = 0f;  // default
+        float angularSpeed = 0f;  // default
+        if (Input.GetKey(KeyCode.UpArrow)) linearSpeed = 1f;  // m/s
+        if (Input.GetKey(KeyCode.DownArrow)) linearSpeed = -0.5f;  // m/s
+        if (Input.GetKey(KeyCode.LeftArrow)) angularSpeed = -60f;  // deg/s
+        if (Input.GetKey(KeyCode.RightArrow)) angularSpeed = 60f;  // deg/s
+
+        // Rotate around y - axis
+        transform.Rotate(0, angularSpeed * Time.fixedDeltaTime, 0);
+
+        // Move forward / backward
+        transform.Translate(0, 0, linearSpeed * Time.fixedDeltaTime);
+    }
+}
